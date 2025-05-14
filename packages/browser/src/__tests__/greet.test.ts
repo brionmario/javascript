8cc7419d1e129af7bbd336ecf7e33d73750887ee
Brion Mario <info@brionmario.com>
2025-05-14 14:33:54 +0530

    chore(browser): init `browser` package

---
+++ packages/browser/src/__tests__/greet.test.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import {Locator, page} from '@vitest/browser/context';
+import {describe, expect, test} from 'vitest';
+import greet from '../greet';
+
+describe('greet', () => {
+  test('should return the proper greeting', async () => {
+    const parent: HTMLDivElement = greet('World');
+    document.body.appendChild(parent);
+
+    const element: Locator = page.getByText('Hello World!');
+    expect(element).toBeInTheDocument();
+  });
+});
